8728e00f86105788c9bceeb20f7f9d3385011ee3
Michael Quinton <248752364+michael-quinton@users.noreply.github.com>
2026-06-11 14:49:50 +0100

    refactor: indentation and formatting

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,8 +3,31 @@
 # ------------------------------------------------------------------------------
 locals {
   k3s = {
-    "k3s-master-1" = { os_code_name = "bookworm", octetIP = "201", vcpu = 2, memoryMB = 2048, incGB = 20 },
-    "k3s-worker-1" = { os_code_name = "bookworm", octetIP = "202", vcpu = 1, memoryMB = 1536, incGB = 20 },
-    "k3s-worker-2" = { os_code_name = "bookworm", octetIP = "203", vcpu = 1, memoryMB = 1536, incGB = 20 },
+    "k3s-master-1" = {
+      os_code_name = "bookworm"
+      octetIP      = "201"
+      mac          = "52:54:00:00:01:c9"
+      vcpu         = 2
+      memoryMB     = 2048
+      incGB        = 20
+    }
+
+    "k3s-worker-1" = {
+      os_code_name = "bookworm"
+      octetIP      = "202"
+      mac          = "52:54:00:00:01:ca"
+      vcpu         = 1
+      memoryMB     = 1536
+      incGB        = 20
+    }
+
+    "k3s-worker-2" = {
+      os_code_name = "bookworm"
+      octetIP      = "203"
+      mac          = "52:54:00:00:01:cb"
+      vcpu         = 1
+      memoryMB     = 1536
+      incGB        = 20
+    }
   }
 }
